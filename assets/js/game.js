50d13ab7a45d676462ffd03c3ee4562f434fcfce
Kevin Lawler <kevinrlawler@gmail.com>
2016-12-07 21:57:09 -0500

    start of javascript and adding CSS and HTML id's

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,11 +1,24 @@
 $(document).ready(function() {
 
 
-
 	var counter = 0 
 
+	function randomNumber() {
+		var randomNumber = Math.floor(Math.random()*112);
+		$("#guessbox").html("<h1>" + randomNumber + "</h1>");
+		console.log(randomNumber);
+	};
+
+	function crystalNumber() {
+		var randomNumber = Math.floor(Math.random()*9);
+		document.getElementById("crystal-1").value = randomNumber;
+		// $("#crystal-1").value(randomNumber);
+		console.log(randomNumber);
+	};
 
 
 
 
+	crystalNumber();
+	randomNumber();
 });
